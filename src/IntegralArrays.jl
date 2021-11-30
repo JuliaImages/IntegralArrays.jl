00ed391f87c54e1b0ff7ab3dbdf0de0650266f78
JohnnyChen <johnnychen94@hotmail.com>
2021-11-30 19:53:50 +0800

    more verbose documentation

--- src/IntegralArrays.jl
+++ src/IntegralArrays.jl
@@ -6,34 +6,75 @@ using IntervalSets
 export IntegralArray
 
 """
+    iA = IntegralArray(A)
+
+Construct the integral array of the input array `A`.
+
+The integral array is calculated by assigning to each cell the sum of all cells above it and
+to its left, i.e. the rectangle from origin point to the pixel position. For example, in 1-D
+case, `iA[i] = sum(A[1:i])` if the vector `A`'s origin is 1.
+
+```jldoctest; setup=:(using IntegralArrays)
+julia> A = reshape(1:25, 5, 5)
+5×5 reshape(::UnitRange{$Int}, 5, 5) with eltype $Int:
+ 1   6  11  16  21
+ 2   7  12  17  22
+ 3   8  13  18  23
+ 4   9  14  19  24
+ 5  10  15  20  25
+
+julia> iA = IntegralArray(A)
+5×5 IntegralArray{$Int, 2, $(Matrix{Int})}:
+  1   7   18   34   55
+  3  16   39   72  115
+  6  27   63  114  180
+ 10  40   90  160  250
+ 15  55  120  210  325
+
+julia> iA[3] == sum(A[1:3, 1:3])
+true
+
+julia> iA[3..5, 3..5] == sum(A[3:5, 3:5])
+true
 ```
-integral_array = IntegralArray(A)
+
+The closed interval `a..b` is used to support the integral array `iX` with a different
+indexing semantic; `iX[a:b]` gives a subarray of `iX`, while `iX[a..b]` calculates the sum
+of original array `X` over region `a:b`. `a ± σ` (can be typed by `\\pm<tab>`) is a
+convenient constructor of the closed interval `a-σ..a+σ`.
+
+When one needs to compute the sum/average of all blocks extracted from an image, pre-building
+the integral array usually provides a more efficient computation.
+
+```julia
+using BenchmarkTools, IntegralArrays
+
+# simplified 3x3 mean filter; only for demo purpose
+function mean_filter_naive!(out, X)
+    Δ = CartesianIndex(1, 1)
+    for i in CartesianIndex(2, 2):CartesianIndex(size(X).-1)
+        block = @view X[i-Δ: i+Δ]
+        out[i] = mean(block)
+    end
+    return out
+end
+function mean_filter_integral!(out, X)
+    iX = IntegralArray(X)
+    for i in CartesianIndex(2, 2):CartesianIndex(size(X).-1)
+        x, y = i.I
+        out[i] = iX[x±1, y±1]/9
+    end
+    return out
+end
+
+X = Float32.(rand(1:5, 64, 64));
+m1 = copy(X);
+m2 = copy(X);
+
+@btime mean_filter_naive!(\$m1, \$X); # 65.078 μs (0 allocations: 0 bytes)
+@btime mean_filter_integral!(\$m2, \$X); # 12.161 μs (4 allocations: 16.17 KiB)
+m1 == m2 # true
 ```
-
-Returns the integral array of an array. The integral array is calculated by assigning
-to each cell the sum of all cells above it and to its left, i.e. the rectangle from
-(1, 1) to the pixel. An integral array is a data structure which helps in efficient
-calculation of sum of pixels in a rectangular subset of an array.
-
-```
-sum = integral_array[ytop..ybot, xtop..xbot]
-sum = integral_array[y ± σy, x ± σx]
-```
-
-The sum of a window in an array can be directly calculated using four array references of
-the integral array, irrespective of the size of the window, given the `yrange` and `xrange`
-of the window. Given an integral array -
-
-        A - - - - - - B -
-        - * * * * * * * -
-        - * * * * * * * -
-        - * * * * * * * -
-        - * * * * * * * -
-        - * * * * * * * -
-        C * * * * * * D -
-        - - - - - - - - -
-
-The sum of pixels in the area denoted by * is given by S = D + A - B - C.
 """
 struct IntegralArray{T, N, A} <: AbstractArray{T, N}
     data::A
